ccfa32e840d180e85edae34e3ee98ddfe4aa2e6f
Bruno Ribeiro <contact@nullscope.com>
2023-04-30 22:40:19 +0100

    Initial Code Commit (#1)
    * Removed template files and settings
    
    * Added Auto switching and saving logic for Graphics Settings
    
    * Updated readme

--- Plugin.cs
+++ Plugin.cs
@@ -1,41 +1,38 @@
+using System.IO;
 using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
-using COTL_API.CustomInventory;
-using CotLTemplateMod.Items;
-using System.IO;
-using static InventoryItem;
-using COTL_API.CustomFollowerCommand;
-using CotLTemplateMod.CustomFollowerCommands;
+using Newtonsoft.Json;
 
-namespace CotLTemplateMod
+namespace CotLAutoGraphicsSettings
 {
     [BepInPlugin(PluginGuid, PluginName, PluginVer)]
-    [BepInDependency("io.github.xhayper.COTL_API")]
     [HarmonyPatch]
     public class Plugin : BaseUnityPlugin
     {
-        public const string PluginGuid = "IngoH.cotl.CotLTemplateMod";
-        public const string PluginName = "CotLTemplateMod";
+        public const string PluginGuid = "io.github.NullScope.CotLAutoGraphicsSettings";
+        public const string PluginName = "CotLAutoGraphicsSettings";
         public const string PluginVer = "1.0.0";
 
         internal static ManualLogSource Log;
         internal readonly static Harmony Harmony = new(PluginGuid);
 
         internal static string PluginPath;
+        internal static ConfigFile Cfg;
 
-        internal static ITEM_TYPE ExampleItem;
+        internal static ConfigEntry<string> DefaultGraphicsConfig = null!;
+        internal static ConfigEntry<string> CultBaseGraphicsConfig = null!;
 
-        internal static FollowerCommands FollowerCommand;
+        internal static SettingsData.GraphicsSettings DefaultGraphics = null!;
+        internal static SettingsData.GraphicsSettings CultBaseGraphics = null!;
 
         private void Awake()
         {
             Plugin.Log = base.Logger;
+            Plugin.Cfg = base.Config;
 
-            PluginPath = Path.GetDirectoryName(Info.Location);
-
-            ExampleItem = CustomItemManager.Add(new ExampleItem());
-            FollowerCommand = CustomFollowerCommandManager.Add(new ExampleFollowerCommand());
+            Plugin.PluginPath = Path.GetDirectoryName(Info.Location);
         }
 
         private void OnEnable()
@@ -48,6 +45,49 @@ namespace CotLTemplateMod
         {
             Harmony.UnpatchSelf();
             Logger.LogInfo($"Unloaded {PluginName}!");
+        }
+
+        public static SettingsData.GraphicsSettings CloneSettings (SettingsData.GraphicsSettings settings)
+		{
+            var newSettings = new SettingsData.GraphicsSettings(settings);
+            // For some reason the constructor does not copy over the AntiAliasing option, so we set it here.
+            newSettings.AntiAliasing = settings.AntiAliasing;
+
+            return newSettings;
+        }
+
+        public static void SetupConfig()
+		{
+            DefaultGraphicsConfig = Cfg.Bind("Graphics", "Default", JsonConvert.SerializeObject(SettingsManager.Settings.Graphics), "Default Graphics Settings");
+            CultBaseGraphicsConfig = Cfg.Bind("Graphics", "Base", JsonConvert.SerializeObject(SettingsManager.Settings.Graphics), "Cult Base Graphics Settings");
+            DefaultGraphics = JsonConvert.DeserializeObject<SettingsData.GraphicsSettings>(DefaultGraphicsConfig.Value);
+            CultBaseGraphics = JsonConvert.DeserializeObject<SettingsData.GraphicsSettings>(CultBaseGraphicsConfig.Value);
+        }
+
+        public static void SaveDefaultGraphics()
+		{
+            DefaultGraphicsConfig.BoxedValue = JsonConvert.SerializeObject(SettingsManager.Settings.Graphics);
+            DefaultGraphics = CloneSettings(SettingsManager.Settings.Graphics);
+		}
+
+        public static void SaveBaseGraphics()
+		{
+            CultBaseGraphicsConfig.BoxedValue = JsonConvert.SerializeObject(SettingsManager.Settings.Graphics);
+            CultBaseGraphics = CloneSettings(SettingsManager.Settings.Graphics);
+        }
+
+        public static void ApplySettings(FollowerLocation location = FollowerLocation.None)
+		{
+            if (location == FollowerLocation.Base)
+            {
+                SettingsManager.Settings.Graphics = CloneSettings(CultBaseGraphics);
+            }
+            else
+            {
+                SettingsManager.Settings.Graphics = CloneSettings(DefaultGraphics);
+            }
+
+            SettingsManager.Instance.ApplySettings();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,28 @@
-# CotLTemplateMod
-Cult of the Lamb Template Mod
+# CotLAutoGraphicsSettings
 
-**This is the Full Template Branch** (including template code for the API and patches)  
-**!! When using this template with Visual Studio, make sure to run `dotnet restore` from the terminal (View -> Terminal)**
+Cult of the Lamb Auto Graphics Settings Mod is a mod that allows you to set different graphics settings for your Cult Base and for the rest of the game. When moving between between places (e.g between your Cult Base and going into a Crusade or vice-versa), this mod will automatically set the predefined graphics settings for that place.
+
+## Why?
+
+The Cult Base can become very resource heavy as you progress in the game. As such, low power or portable devices (such as the Steam Deck) will begin to slow down and heat up. While I played on my Steam Deck I was constantly swapping to the low preset at 30 fps while in the base to prevent the Steam Deck fan from going full blast and to preserve battery life.
+This mod was born to do this automatically for me and anyone interested in doing the same.
+
+# Requirements
+
+- [BepInEx](https://docs.bepinex.dev/articles/user_guide/installation/index.html) (As of writing v5.4.21)
+
+## How to Install
+
+- Extract BepInEx into the Cult of the Lamb's directory (e.g `steamapps/common/Cult of the Lamb`).
+- Launch the game once so that BepInEx can create the necessary files and folders.
+- Place the DLL from the latest [release](https://github.com/NullScope/CotLAutoGraphicsSettings/releases) into BepInEx's plugins folder (e.g `steamapps/common/Cult of the Lamb/BepInEx/plugins`).
+
+## How to Use
+
+On the first run, the mod will automatically save the settings currently used as both the default and the Cult Base settings.
+
+To change the Cult Base settings, go to your Cult Base and change the Graphics Settings in the Game's UI as usual, to the settings you prefer.
+
+To change the Default settings (i.e the settings used outside of the Cult Base) simply go to somewhere outside of the Cult Base (e.g Start a crusade or go to the Lighthouse) and change your Graphics Settings in the Game's UI.
+
+**Note:** Both the Church and the room where you select which Crusade to start (the room with the 4 doors) count as part of the Cult Base.
